200acc4ed53f9573b94e0117094bb007b9459a32
BrunoBlanco011 <brunoblalu11@gmail.com>
2024-10-30 00:42:48 -0600

    avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,10 @@ import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { CardComponent } from './components/card/card.component';
 import { TiendaComponent } from './pages/tienda/tienda.component';
+import { LoginComponent } from "./pages/login/login.component";
+import { LoginviveroComponent } from "./pages/loginvivero/loginvivero.component";
+import { RegistroComponent } from "./pages/registro/registro.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
@@ -13,8 +17,12 @@ import { TiendaComponent } from './pages/tienda/tienda.component';
     HeaderComponent,
     FooterComponent,
     CardComponent,
-    TiendaComponent
-  ],
+    TiendaComponent,
+    LoginComponent,
+    LoginviveroComponent,
+    RegistroComponent,
+    HomeComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/pages/loginvivero/loginvivero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loginvivero',
+  standalone: true,
+  imports: [],
+  templateUrl: './loginvivero.component.html',
+  styleUrl: './loginvivero.component.scss'
+})
+export class LoginviveroComponent {
+
+}
