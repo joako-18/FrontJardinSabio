d0f3ba1afaf85e4ee3332a234077a51039edea95
joako-18 <joako1327@gmai.com>
2024-10-30 09:35:01 -0600

    solucion de errores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,12 @@ import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { CardComponent } from './components/card/card.component';
 import { TiendaComponent } from './pages/tienda/tienda.component';
-<<<<<<< HEAD
 import { LoginComponent } from "./pages/login/login.component";
 import { LoginviveroComponent } from "./pages/loginvivero/loginvivero.component";
 import { RegistroComponent } from "./pages/registro/registro.component";
 import { HomeComponent } from "./pages/home/home.component";
-=======
 import { CarritoComponent } from './pages/carrito/carrito.component';
-<<<<<<< HEAD
 import { ComunidadComponent } from './pages/comunidad/comunidad.component';
-=======
->>>>>>> 3a8625730c52e540030d1be171f64bf9fdcd7dee
->>>>>>> 7b1d538032a9da42e17d335a4a6c41ee4bfaf8dc
 
 @Component({
   selector: 'app-root',
@@ -26,21 +20,14 @@ import { ComunidadComponent } from './pages/comunidad/comunidad.component';
     FooterComponent,
     CardComponent,
     TiendaComponent,
-<<<<<<< HEAD
     CarritoComponent,
-    ComunidadComponent
-=======
-<<<<<<< HEAD
+    ComunidadComponent,
     LoginComponent,
     LoginviveroComponent,
     RegistroComponent,
-    HomeComponent
-],
-=======
+    HomeComponent,
     CarritoComponent
->>>>>>> 7b1d538032a9da42e17d335a4a6c41ee4bfaf8dc
-  ],
->>>>>>> 3a8625730c52e540030d1be171f64bf9fdcd7dee
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
